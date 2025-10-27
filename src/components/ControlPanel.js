23643e9aca8cf078db222bdf9b461b4ea1243295
leyny037 <leyny037@mymail.unisa.edu.au>
2025-10-27 17:45:19 +1030

    Change button design

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,12 +1,65 @@
+import { useState } from "react";
+import "./ControlPanel.css";
+
 export default function ControlPanel() {
+    const [activeButton, setActiveButton] = useState(null);
+
+    const handleClick = (id) => {
+        setActiveButton(id);
+
+        // Remove glow after 400ms
+        setTimeout(() => setActiveButton(null), 400);
+
+        switch (id) {
+            case "play":
+                console.log("Play clicked");
+                break;
+            case "stop":
+                console.log("Stop clicked");
+                break;
+            case "process":
+                console.log("Preprocess clicked");
+                break;
+            case "process_play":
+                console.log("Proc & Play clicked");
+                break;
+            default:
+                break;
+        }
+    };
+
     return (
-        <div className="col-md-4">
+        <div className="col-md-4 control-panel">
             <nav>
-                <button id="process" className="btn btn-outline-primary m-1">Preprocess</button>
-                <button id="process_play" className="btn btn-outline-primary m-1">Proc & Play</button>
+                <button
+                    id="process"
+                    className={activeButton === "process" ? "active-glow" : ""}
+                    onClick={() => handleClick("process")}
+                >
+                    Preprocess
+                </button>
+                <button
+                    id="process_play"
+                    className={activeButton === "process_play" ? "active-glow" : ""}
+                    onClick={() => handleClick("process_play")}
+                >
+                    Proc & Play
+                </button>
                 <br />
-                <button id="play" className="btn btn-outline-success m-1">Play</button>
-                <button id="stop" className="btn btn-outline-danger m-1">Stop</button>
+                <button
+                    id="play"
+                    className={activeButton === "play" ? "active-glow" : ""}
+                    onClick={() => handleClick("play")}
+                >
+                    Play
+                </button>
+                <button
+                    id="stop"
+                    className={activeButton === "stop" ? "active-glow" : ""}
+                    onClick={() => handleClick("stop")}
+                >
+                    Stop
+                </button>
             </nav>
         </div>
     );
