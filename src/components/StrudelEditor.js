fda8aa8f2d27b2c4c4a9d7723c6bd7aae44568a7
leyny037 <leyny037@mymail.unisa.edu.au>
2025-11-17 20:11:43 +1030

    Match the same layout to strudel text editor

--- src/components/StrudelEditor.js
+++ src/components/StrudelEditor.js
@@ -1,6 +1,19 @@
 export default function StrudelEditor() {
     return (
-        <div className="col-md-8 strudel-editor" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+        <div className="col-md-8 strudel-editor" style={{
+            maxHeight: '50vh',
+            overflowY: 'auto',
+            marginTop: "25px",
+            fontSize: "0.8rem",
+            lineHeight: "1.2",
+            textAlign: "left",
+            padding: "10px",
+            border: "1px solid #0ff",
+            borderRadius: "6px",
+            backgroundColor: "rgba(0,0,0,0.35)",
+            width: "100%",
+            boxSizing: "border-box"
+        }}>
             <label htmlFor="proc" className="editor-label">Text to preprocess:</label>
             <textarea className="form-control" rows="15" id="proc"></textarea>
         </div>
